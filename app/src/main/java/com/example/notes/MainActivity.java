c11d4410183ea41f71154445f1ebb0218e39b03c
Arnab Chanda <arnabchanda964@gmail.com>
2019-04-13 21:48:16 +0530

    BottomNavigationDrwer changing items smoothly

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +23,23 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.add_notes:
-                    mTextMessage.setText(R.string.title_home);
+                    button.setText(R.string.notes_message);
+                    textView.setVisibility(View.VISIBLE);
+                    textView2.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.remainder:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    textView2.setVisibility(View.VISIBLE);
+                    textView.setVisibility(View.INVISIBLE);
+                    button.setText(R.string.remainder_message);
+
                     return true;
             }
             return false;
         }
     };
-
+    Button button;
+    TextView textView;
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +51,12 @@ public class MainActivity extends AppCompatActivity {
         TextView appname = findViewById(R.id.appname);
         appname.setText("Notes");
 
-
-        mTextMessage = (TextView) findViewById(R.id.message);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.Notes_message);
+        textView2 = findViewById(R.id.remainder_message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.add_notes);
     }
 
 }
