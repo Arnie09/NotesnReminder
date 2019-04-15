476b6d97549e7a967a82094045f55763ddbd7c07
Arnab Chanda <arnabchanda964@gmail.com>
2019-04-15 15:45:19 +0530

    Set up basic layout off NoteTaking

--- app/src/main/java/com/example/notes/NoteTaking.java
+++ app/src/main/java/com/example/notes/NoteTaking.java
@@ -2,6 +2,12 @@ package com.example.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NoteTaking extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class NoteTaking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_taking);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView appname = findViewById(R.id.appname);
+        appname.setText("Notes and Remainder");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.notes_option_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.Save){
+            //make database entry
+            Toast.makeText(this, "Item Saved", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
