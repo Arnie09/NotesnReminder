edc693195a69612710512e6f56bd2a5425048aef
Arnab Chanda <arnabchanda964@gmail.com>
2019-04-15 15:32:13 +0530

    Optimised the layout files and cleaned code

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,23 +26,36 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.add_notes:
-                    button.setText(R.string.notes_message);
-                    timePicker.setVisibility(View.INVISIBLE);
-                    recyclerView.setVisibility(View.VISIBLE);
+
+                    Noteslayout.setVisibility(View.VISIBLE);
+                    Remainderlayout.setVisibility(View.INVISIBLE);
+                    TakeNotes.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(getApplicationContext(),NoteTaking.class));
+                        }
+                    });
                     return true;
                 case R.id.remainder:
-                    timePicker.setVisibility(View.VISIBLE);
-                    recyclerView.setVisibility(View.INVISIBLE);
-                    button.setText(R.string.remainder_message);
-
+                    Noteslayout.setVisibility(View.INVISIBLE);
+                    Remainderlayout.setVisibility(View.VISIBLE);
+                    StartTimer.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(MainActivity.this, "Timer Started", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                     return true;
             }
             return false;
         }
     };
-    Button button;
+    Button TakeNotes;
+    Button StartTimer;
     RecyclerView recyclerView;
     TimePicker timePicker;
+    View Noteslayout;
+    View Remainderlayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,11 +65,14 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         TextView appname = findViewById(R.id.appname);
-        appname.setText("Notes");
+        appname.setText("Notes and Remainder");
 
-        button = findViewById(R.id.button);
+        Noteslayout = findViewById(R.id.notesItems);
+        Remainderlayout = findViewById(R.id.remainderItems);
+        TakeNotes = findViewById(R.id.button);
         recyclerView = findViewById(R.id.RecyclerView);
         timePicker = findViewById(R.id.TimePicker);
+        StartTimer = findViewById(R.id.timerStart);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.add_notes);
